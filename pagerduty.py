dbbec0eb97adf724988f0a6657929bbafacf9537
nahusznaj <nsznaj@gmail.com>
2021-02-09 10:56:14 -0300

    Update pagerduty.py
    mejoradooo

--- pagerduty.py
+++ pagerduty.py
@@ -4,7 +4,7 @@ import pprint
 import time 
 import hmac 
 import hashlib
-import urllib.parse 
+import urllib
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,46 +15,59 @@ slack_signing_secret = os.getenv("slack_signing_secret")
 
 app = Flask(__name__)
 
-# poll PD to obtain a list with all the schedules.
-payload_schedules =  {'total':'true', 'limit':100}
-schedules_id = []
+def obtain_schedule_ids():
+    # poll PD to obtain a list with all the schedules.
+    payload_schedules =  {'total':'true', 'limit':100}
+    schedules_list = []
 
-rr = requests.get('https://api.pagerduty.com/schedules', headers = {"Authorization":'Token token='+PD_token},  params = payload_schedules)
+    rr = requests.get('https://api.pagerduty.com/schedules', headers = {"Authorization":'Token token='+PD_token},  params = payload_schedules)
 
-schedules_page_response = rr.json()
+    schedules_page_response = rr.json()
 
-all_schedules_list = schedules_page_response['schedules']
+    all_schedules_list = schedules_page_response['schedules']
 
-for j in all_schedules_list:
-    schedules_id.append(j['id'])
+    for item in all_schedules_list:
+        schedules_list.append(item['id'])
 
-def signature_verification(timestamp, slack_payload_dict, signign_secret, incoming_slack_signature):
-    incoming_payload = "&".join(['='.join([key, urllib.parse.quote(val, safe='')]) for key, val in slack_payload_dict.items()]) # we're parsing the payload to replace characters / -> %2, : -> %3A
+    return schedules_list 
+
+schedules_id = obtain_schedule_ids() 
+
+def verify_signature(headers, slack_payload, known_signature):
+    timestamp = headers['X-Slack-Request-Timestamp'] # another var of absolute time in headers sent by Slack
+    slack_signature = headers['X-Slack-Signature'] # this is the signature that Slack sends along the POST, in the headers.
+    
+    # this is the payload in the body of the request from Slack to our app when the slash command was executed in Slack
+    dict_slack = slack_payload.to_dict() 
+
+    incoming_payload = urllib.parse.urlencode(dict_slack)
 
     ### compose the message for our side's sha256 signature
     sig_basestring = 'v0:' + timestamp + ':' + incoming_payload
     sig_basestring = sig_basestring.encode('utf-8')
 
     ### Signing Secret provided by Slack upon creating the App, encoded:
-    signing_secret = slack_signing_secret.encode('utf-8')
+    signing_secret = known_signature.encode('utf-8')
     
     ## create our signature
     my_signature = 'v0=' + hmac.new(signing_secret, sig_basestring, hashlib.sha256).hexdigest()
 
-    if my_signature == incoming_slack_signature:  
+    if my_signature == slack_signature:  
         return True
     else:
         return False
 
-def user_text_input_verification(input_text):
-    return input_text == ''
+def verify_user_text_input(slack_payload):
+    dict_slack = slack_payload.to_dict() 
+    user_text_input = dict_slack['text']  
+    return user_text_input == ''
 
 def obtain_oncalls_results(schedules_list):
     ## read the schedules for each scheduleID item 
     payload = {'time_zone':'America/Argentina/Buenos_Aires', 'total':'true', 'schedule_ids[]':schedules_list, 'limit':100} #scheduleIDs 
     r = requests.get('https://api.pagerduty.com/oncalls', headers = {"Authorization":'Token token='+PD_token},  params = payload)
     
-    pager_response = json.loads(r.text)
+    pager_response = r.json()
     
     ## create a dict with each schedule and the oncall user
     information_dict = {}
@@ -68,13 +81,8 @@ def obtain_oncalls_results(schedules_list):
 
     return output 
 
-@app.route('/', methods=['POST']) 
-def all_schedules():
-
-    slack_headers = request.headers
-
-    slack_signature = slack_headers['X-Slack-Signature'] # this is the signature that Slack sends along the POST, in the headers.
-    timestamp = slack_headers['X-Slack-Request-Timestamp'] #absolute time in headers sent by Slack
+def verify_replay_attack(headers):
+    timestamp = headers['X-Slack-Request-Timestamp'] #absolute time in headers sent by Slack
 
     ### Security measure ###
     # The request timestamp is more than five minutes from local time.
@@ -82,24 +90,24 @@ def all_schedules():
     if timestamp is not None:
         timestamp = float(timestamp)
         if abs(time.time() - timestamp) > 60 * 5:
-            return '', 403  
+            return '', 403
+        else:
+            pass
 
-    timestamp2 = slack_headers['X-Slack-Request-Timestamp'] # another var of absolute time in headers sent by Slack
-    
-    slack_payload = request.form
-    
-    # this is the payload in the body of the request from Slack to our app when the slash command was executed in Slack
-    dict_slack = slack_payload.to_dict() 
+@app.route('/', methods=['POST']) 
+def all_schedules():
+    ### Security measure ###
+    # verify there's no replay attack
+    verify_replay_attack(request.headers)
+  
 
     ### Security measure ###
     # verify the signature, if successful this will be True
-    signature_validation_result = signature_verification(timestamp2, dict_slack, slack_signing_secret, slack_signature) 
+    signature_validation_result = verify_signature(request.headers, request.form, slack_signing_secret) 
     
-    ### Security measure ###
-    # declare the incoming request's text input (hoping it'll be empty!)
-    user_text_input = dict_slack['text']  
-    # verify no input text was passed, if successful this will be True
-    input_text_verif_result = user_text_input_verification(user_text_input) 
+    # ### Security measure ###
+    # # verify no input text was passed, if successful this will be True
+    input_text_verif_result = verify_user_text_input(request.form) 
     
     # if the signatures match and no text was passed, let's then run this show!
     if signature_validation_result and input_text_verif_result: 
